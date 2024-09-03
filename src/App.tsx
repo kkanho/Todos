cd655846e5be1a407422b3442bb79751fba64432
kkanho <kanho3058@gmail.com>
2024-09-04 00:07:56 +0800

    feat: color for alert

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ type Todo = {
   id: string
   value: string
   done: boolean
+  start_time: number
+  finish_time: number | undefined
 }
 
 function App() {
@@ -13,9 +15,9 @@ function App() {
     const [isInitialLoad, setIsInitialLoad] = useState(true)
 
     useEffect(() => {
-      const stored = localStorage.getItem('Todos');
+      const stored = localStorage.getItem('Todos')
       if (stored) {
-        setTodos(JSON.parse(stored));
+        setTodos(JSON.parse(stored))
       }
       setIsInitialLoad(false)
     }, [])
@@ -27,7 +29,17 @@ function App() {
     }, [todos, isInitialLoad])
 
     const handleAddTodo = () => {
-      setTodos((prev) => [...prev, {id: uuidv4(), value: todo, done: false}])
+      const timestamp = +new Date()
+
+      const newTodo = {
+        id: uuidv4(),
+        value: todo,
+        done: false,
+        start_time: timestamp,
+        finish_time: undefined
+      }
+
+      setTodos((prev) => [newTodo, ...prev])
       setTodo("")
     }
 
@@ -36,17 +48,23 @@ function App() {
     }
 
     const handleDone = (id: string): void => {
+      const timestamp = +new Date()
+
       setTodos((prev) => (
         prev.map((todo) =>
-          todo.id === id ? { ...todo, done: !todo.done } : todo
+          todo.id === id ? { ...todo, done: !todo.done, finish_time: timestamp } : todo
         )
       ))
     }
 
+    const daysToMilliseconds = (days: number):number => {
+      return days * 24 * 60 * 60 * 1000
+    }
+
     return (
         <div className="h-[100dvh] flex flex-col">
-            <div className="mx-auto sm:m-auto container border p-4 max-w-xl rounded-md shadow-md">
-                <div className="flex flex-col gap-4">
+            <div className="mx-auto sm:m-auto container max-h-[100dvh] border p-4 max-w-xl rounded-md shadow-md">
+                <div className="flex flex-col h-full">
                     <div className="flex gap-2">
                         <input
                             type="text"
@@ -67,25 +85,30 @@ function App() {
                             </svg>
                         </button>
                     </div>
-                    {
-                      todos.map((todo) => (
-                        <div key={todo.id} className={`flex gap-2 w-full ${todo.done? "bg-green-400 rounded" :"" }`}>
-                          <div className={`todo flex-1 px-3 py-2 overflow-auto cursor-pointer ${todo.done? "line-through": ""}`} onClick={() => handleDone(todo.id)}>
-                            {todo.value}
+                    <div className="flex flex-col h-full overflow-auto">
+                      {
+                        todos.map((todo) => (
+                          <div key={todo.id} className={`flex gap-2 w-full mt-4 rounded ${todo.done? "bg-green-400" :
+                              (+new Date() - todo.start_time > daysToMilliseconds(3))? "bg-red-400":
+                              (+new Date() - todo.start_time > daysToMilliseconds(2))? "bg-yellow-400": "" }`}>
+                            <div className={`todo flex-1 px-3 py-2 overflow-auto cursor-pointer ${todo.done? "line-through": ""}`} onClick={() => handleDone(todo.id)}>
+                              {todo.value}
+                            </div>
+                            <div className="remove m-auto">
+                              <button
+                                type="button"
+                                aria-label="Remove todo"
+                                onClick={() => handleRemoveTodo(todo.id)}
+                                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border-none bg-inherit hover:opacity-70 h-10 w-10"
+                              >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-plus rotate-45 hover:rotate-[315deg] hover:text-red-600 duration-1000"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
+                              </button>
+                            </div>
                           </div>
-                          <div className="remove m-auto">
-                            <button
-                              type="button"
-                              aria-label="Remove todo"
-                              onClick={() => handleRemoveTodo(todo.id)}
-                              className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border-none bg-inherit hover:opacity-70 h-10 w-10"
-                            >
-                              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-plus rotate-45 hover:rotate-[315deg] hover:text-red-600 duration-1000"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
-                            </button>
-                          </div>
-                        </div>
-                      ))
-                    }
+                        ))
+                      }
+                    </div>
+
                 </div>
             </div>
         </div>
