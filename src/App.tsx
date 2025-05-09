748717203e7081a45054ced748cb0d39e2acf6d0
kkanho <kanho3058@gmail.com>
2025-05-09 16:17:01 +0800

    fix: variant on routine

--- src/App.tsx
+++ src/App.tsx
@@ -172,7 +172,7 @@ function App() {
                             </Button>
                           : tab === "routine"?
                             <Drawer.Root open={isOpen} onOpenChange={setIsOpen} dismissible>
-                              <Drawer.Trigger onClick={() => setIsOpen(true)} className={`${buttonVariants({ variant: "outline", size: "icon" })} inline-flex items-center justify-center whitespace-nowrap hover:opacity-70`}>
+                              <Drawer.Trigger onClick={() => setIsOpen(true)} className={`${buttonVariants({ variant: "default", size: "icon" })} inline-flex items-center justify-center whitespace-nowrap hover:opacity-70`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-send">
                                   <path d="m22 2-7 20-4-9-9-4Z" />
                                   <path d="M22 2 11 13" />
